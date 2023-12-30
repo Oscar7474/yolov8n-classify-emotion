f766b1bb1494fe2a940aa1aea7069c77e91cc7a2
Chin-An Kuo <133106823+Oscar7474@users.noreply.github.com>
2023-12-30 23:29:41 +0800

    Add files via upload

---
+++ cam.py
@@ -0,0 +1,18 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO(r'C:\Users\Kuo\Downloads\last.pt')
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    success, frame = cap.read()
+
+    if success:
+        results = model(frame)
+        annotated_frame = results[0].plot()
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
